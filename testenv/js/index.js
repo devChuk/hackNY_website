008abfc25ebadfc4388414544511a2f1516127a0
root <brian_chuk@yahoo.com>
2015-09-12 14:08:05 -0400

    responsive on load

--- testenv/js/index.js
+++ testenv/js/index.js
@@ -1,11 +1,34 @@
+var logoUnits = {
+	hWidth: 512,
+	hPR: 38,
+	nyPop: 190,
+	nyEnd: 179
+};
+
 function slide() {
-	$( "#hack" ).animate({width: "512px", "padding-right": 38});
+	$( "#hack" ).animate({width: logoUnits.hWidth + "px", "padding-right": logoUnits.hPR});
 }
 
-setTimeout(function() {
-    $( "#N" ).css('visibility','visible').animate({width:"190px", 'padding-right':2, "padding-left":0}, 400).animate({width:"179px"}, 200);
-}, 1000);
+$(function() {
+	if (window.innerWidth < 992) {
+		var logoWidth = 700;
+		if (window.innerWidth < 768 && window.innerWidth >= 480) {
+			logoWidth = 450;
+		} else if (window.innerWidth < 480) {
+			logoWidth = 350;
+		}
+		for (var key in logoUnits) {
+			logoUnits[key] = Math.round(logoWidth * logoUnits[key] / 918);
+			console.log(logoWidth);
+			console.log(key + logoUnits[key]);
+		}
+	}
 
-setTimeout(function() {
-    $( "#Y" ).css('visibility','visible').animate({width:"190px", 'padding-right':0, "padding-left":2}, 400).animate({width:"179px"}, 200, slide);
-}, 1050);
+	setTimeout(function() {
+	    $( "#N" ).css('visibility','visible').animate({width: logoUnits.nyPop + "px", 'padding-right':2, "padding-left":0}, 400).animate({width: logoUnits.nyEnd + "px"}, 200);
+	}, 1000);
+
+	setTimeout(function() {
+	    $( "#Y" ).css('visibility','visible').animate({width: logoUnits.nyPop + "px", 'padding-right':0, "padding-left":2}, 400).animate({width: logoUnits.nyEnd + "px"}, 200, slide);
+	}, 1050);
+});
