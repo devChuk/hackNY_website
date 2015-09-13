f8db6b8027b0032fcea2f639a3bc4a3db231b390
root <brian_chuk@yahoo.com>
2015-09-13 04:13:45 -0400

    :fire:

--- testenv/js/index.js
+++ testenv/js/index.js
@@ -15,6 +15,9 @@ var layout;
 function slide() {
 	$( "#hack" ).animate({width: logoUnits.hWidth + "px", "padding-right": logoUnits.hPR});
 	$( ".head-text h1" ).fadeIn();
+	$("header").animate({bottom: "60px"}, function () {
+		$(this).css({position: "absolute", bottom: "0px"});
+	});
 }
 
 function screenCheck() {
@@ -24,7 +27,7 @@ function screenCheck() {
 		layout = 2;
 		if (window.innerWidth < 768 && window.innerWidth >= 480) {
 			logoWidth = 450;
-			layout = 1
+			layout = 1;
 		} else if (window.innerWidth < 480) {
 			logoWidth = 350;
 			layout = 0;
@@ -45,11 +48,41 @@ $(function() {
 
 	setTimeout(function() {
 	    $( "#N" ).css('visibility','visible').animate({width: logoUnits.nyPop + "px", 'padding-right':2, "padding-left":0}, 400).animate({width: logoUnits.nyEnd + "px"}, 200);
-	}, 1000);
+	}, 0);
 
 	setTimeout(function() {
 	    $( "#Y" ).css('visibility','visible').animate({width: logoUnits.nyPop + "px", 'padding-right':0, "padding-left":2}, 400).animate({width: logoUnits.nyEnd + "px"}, 200, slide);
-	}, 1050);
+	}, 50);
+
+	var scrollTimeout;
+	$(window).on('scroll', function() {
+    if ($(window).width() >= 786) {
+      clearTimeout(scrollTimeout);
+      scrollTimeout = setTimeout(checkAndMoveNavbarPosition(), 250);
+    }
+  });
+
+  /* move the nav bar as appropriate */
+  var isFixed = false;
+  var navbarHeight;
+  var deltaLocation;
+  recalculateNavbarPosition();
+  function checkAndMoveNavbarPosition() {
+    if (!isFixed && $(window).scrollTop() > deltaLocation) {
+      $('header').css({position: "fixed", top: "0px", bottom: ""});
+      isFixed = true;
+    }
+    else if (isFixed && $(window).scrollTop() < deltaLocation) {
+		$('header').css({position: "absolute", top: "", bottom: "0px"});
+      isFixed = false;
+      $('header').show();
+    }
+  }
+
+  function recalculateNavbarPosition() {
+    navbarHeight = $('#navbar').outerHeight();
+    deltaLocation = $('#splash').outerHeight() - navbarHeight;
+  }
 });
 
 $( window ).resize(function() {
@@ -59,5 +92,4 @@ $( window ).resize(function() {
 			$( "#N, #Y" ).css({width: logoUnits.nyEnd + "px"});
 			$( "#hack" ).css({width: logoUnits.hWidth + "px", "padding-right": logoUnits.hPR})
 	}
-
 });
